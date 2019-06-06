52df654b3632934e669c1631da49528e5aa6633f
1ChayanonSangangam <51090776+1ChayanonSangangam@users.noreply.github.com>
2019-06-06 18:57:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -30,25 +30,25 @@ const url = 'mongodb://127.0.0.1:27017/databaseIOT';
 // Database Name
 const dbName = 'myproject';
 // Use connect method to connect to the server
-for (let i = 0; i < ip.length; i++) {
+// for (let i = 0; i < ip.length; i++) {
     
-    MongoClient.connect('mongodb://158.108.207.134:27017/databaseIOT', function(err, db) {
-        db.collection("tb_iot")
-        .insertOne( { _id:req.body.idDB,url : req.body.strUrl,tableDW:req.body.header,ServiceNameDW:req.body.ServiceNameDW,modifytime:myDate } ,(err,result)=>
-        {
-            if(err)
-            {
-                console.log("error")
-                throw err;
-            }
-            else
-            {
-                console.log("insert complete")
-            }
-        });
+//     MongoClient.connect('mongodb://158.108.207.134:27017/databaseIOT', function(err, db) {
+//         db.collection("tb_iot")
+//         .insertOne( { _id:req.body.idDB,url : req.body.strUrl,tableDW:req.body.header,ServiceNameDW:req.body.ServiceNameDW,modifytime:myDate } ,(err,result)=>
+//         {
+//             if(err)
+//             {
+//                 console.log("error")
+//                 throw err;
+//             }
+//             else
+//             {
+//                 console.log("insert complete")
+//             }
+//         });
 
-    });
-}
+//     });
+// }
 
 app.post('/insert', (req, res) =>{
     MongoClient.connect('mongodb://localhost:27017/databaseIOT', function(err, db) {
