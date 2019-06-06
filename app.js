4269561390cf2ebbe9af656b072d348dd267a215
1ChayanonSangangam <51090776+1ChayanonSangangam@users.noreply.github.com>
2019-06-06 17:55:34 +0700

    ssss

---
+++ app.js
@@ -0,0 +1,54 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const ip =[
+    "158.108.207.134",
+    "158.108.207.207",
+    "158.108.207.183"
+]
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017/databaseIOT';
+ 
+// Database Name
+const dbName = 'myproject';
+// Use connect method to connect to the server
+for (let i = 0; i < ip.length; i++) {
+    
+    MongoClient.connect('mongodb://158.108.207.134:27017/databaseIOT', function(err, db) {
+        db.collection("tb_iot")
+        .insertOne( { _id:req.body.idDB,url : req.body.strUrl,tableDW:req.body.header,ServiceNameDW:req.body.ServiceNameDW,modifytime:myDate } ,(err,result)=>
+        {
+            if(err)
+            {
+                console.log("error")
+                throw err;
+            }
+            else
+            {
+                console.log("insert complete")
+            }
+        });
+
+    });
+}
+
+app.post('/insert', (req, res) =>{
+    MongoClient.connect('mongodb://localhost:27017/databaseIOT', function(err, db) {
+        db.collection("tb_iot")
+        .insertOne( { data1:"xxxx" } ,(err,result)=>
+        {
+            if(err)
+            {
+                console.log("error")
+                throw err;
+            }
+            else
+            {
+                console.log("insert complete")
+            }
+        });
+
+    });
+    
+})
+
---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+const app = require('./app');
+
+
+
+const port = process.env.PORT || 8081
+
+const server = http.createServer(app)
+
+// server.on('listening',function(){
+//     console.log('ok, server is running');
+// });
+
+
+server.listen(port , function(){
+    console.log('Starting node.js on port ' + port)
+})
